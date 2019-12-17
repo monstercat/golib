d30c3b1a100e32d65289a28218d818e18388b0ce
Bryan Cua <bryancua@Bryans-MacBook-Pro.local>
2019-12-17 09:52:33 -0800

    UUID utils

---
+++ string/uuid.go
@@ -0,0 +1,11 @@
+package stringutil
+
+import "regexp"
+
+var (
+	UuidRegexp = regexp.MustCompile("[A-Fa-f0-9]{8}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{12}")
+)
+
+func IsUuid(s string) bool {
+	return UuidRegexp.Match([]byte(s))
+}
---
+++ string/uuid_test.go
@@ -0,0 +1,25 @@
+package stringutil
+
+import "testing"
+
+func TestIsUuid(t *testing.T) {
+
+	tests := []struct{
+		Result bool
+		Test string
+	} {
+		{true, "123e4567-e89b-12d3-a456-426655440000"},
+		{false, "123e4567-e89g-12d3-a456-426655440000"},
+		{true, "123ABCDE-E12F-a000-0033-159392aef039"},
+		{false, "hi"},
+		{false, "ag9320gnaobiz"},
+		{false, "0123456789"},
+	}
+
+	for _, test := range tests {
+		r := IsUuid(test.Test)
+		if r != test.Result {
+			t.Errorf("Expected %s to be %v, got %v", test.Test, test.Result, r)
+		}
+	}
+}
