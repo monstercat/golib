eaa6321d458479af635d79fc59f142084c810473
Colin Kierans <kierans@monstercat.com>
2020-02-06 09:29:54 -0800

    Added query debug

---
+++ db/debug.go
@@ -0,0 +1,19 @@
+package dbUtil
+
+import (
+	"fmt"
+
+	"github.com/Masterminds/squirrel"
+)
+
+func DebugQuery(query *squirrel.SelectBuilder) {
+	DebugQueryPieces(query.ToSql())
+}
+
+func DebugQueryPieces(sql string, args []interface{}, err error) {
+	fmt.Println("sql", sql)
+	for i, arg := range args {
+		fmt.Println("args", i+1, arg)
+	}
+	fmt.Println("err", err)
+}
