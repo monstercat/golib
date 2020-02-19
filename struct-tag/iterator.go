23d7e2a7360cd7b46c8e68400ae5fb0a590c3ffb
lostlevels <lostlevels@users.noreply.github.com>
2020-02-19 09:20:46 -0800

    Add null time and map types.

--- struct-tag/iterator.go
+++ struct-tag/iterator.go
@@ -2,6 +2,7 @@ package struct_tag
 
 import (
 	"database/sql"
+	"encoding/json"
 	"reflect"
 	"time"
 
@@ -22,11 +23,19 @@ func GetData(v reflect.Value) (data interface{}, isZero bool, checked bool) {
 	isZero = v.IsZero()
 
 	switch dt := data.(type) {
+	case map[string]interface{}:
+		isZero = len(dt) == 0
+		var err error
+		data, err = json.Marshal(dt)
+		checked = err == nil
 	case sql.NullString:
 		isZero = !dt.Valid || dt.String == ""
 		checked = true
 	case pgnull.NullString:
 		isZero = !dt.Valid || dt.String == ""
+		checked = true
+	case pgnull.NullTime:
+		isZero = dt.Time.IsZero()
 		checked = true
 	case time.Time:
 		isZero = dt.IsZero()
