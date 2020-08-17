e3c43259cfd6efd1afb57646f31c0a43fec86462
Bryan Cua <bryan@luxe.technology>
2020-08-16 22:56:41 -0700

    maintenance: refactor postgres operators

--- db/postgres/search.go
+++ db/postgres/search.go
@@ -1,8 +1,6 @@
 package pgUtils
 
 import (
-	"fmt"
-
 	"github.com/Masterminds/squirrel"
 
 	"github.com/monstercat/golib/operator"
@@ -11,7 +9,6 @@ import (
 // configuration for different searches.
 type ISearchOperatorConfig interface {
 	Apply(o, rem []operator.Operator, a *Accumulator, prefix string)
-	IsStringType() bool
 	GetKeys() []string
 }
 
@@ -20,6 +17,7 @@ type Accumulator struct {
 	and        squirrel.And
 	remainders squirrel.Or
 }
+
 func (a *Accumulator) ApplyAnd(sql squirrel.Sqlizer) {
 	a.and = append(a.and, sql)
 }
@@ -30,16 +28,19 @@ func (a *Accumulator) ApplyRemainder(sql squirrel.Sqlizer) {
 	a.remainders = append(a.remainders, sql)
 }
 func (a *Accumulator) ApplyToQuery(query *squirrel.SelectBuilder) {
-	// TODO: check if this adds the proper brackets
+	sqlizer := a.GetCondition()
+	if sqlizer != nil {
+		*query = query.Where(sqlizer)
+	}
+}
+func (a *Accumulator) GetCondition() squirrel.Sqlizer {
 	if len(a.and) > 0 {
 		a.or = append(a.or, a.and)
 	}
 	if len(a.remainders) > 0 {
 		a.or = append(a.or, a.remainders)
 	}
-	if len(a.or) > 0 {
-		*query = query.Where(a.or)
-	}
+	return a.or
 }
 
 func ApplyOperators(query *squirrel.SelectBuilder, config []ISearchOperatorConfig, ops operator.Operators, prefix string) {
@@ -51,65 +52,7 @@ func ApplyOperators(query *squirrel.SelectBuilder, config []ISearchOperatorConfi
 	a.ApplyToQuery(query)
 }
 
-
-
-type SearchOperatorConfigBase struct {
-	Keys  []string
-	Field string
-}
-func (c SearchOperatorConfigBase) GetKeys() []string {
-	return c.Keys
-}
-
-type StringSearchOperatorConfigBase struct {
-	SearchOperatorConfigBase
-}
-func (c StringSearchOperatorConfigBase) IsStringType() bool {
-	return true
-}
-
-type SearchOperatorConfigUUID struct{
-	StringSearchOperatorConfigBase
-}
-func (c SearchOperatorConfigUUID) Apply(os, rem []operator.Operator, a *Accumulator, prefix string) {
-	loopOperators(os, a, func(o operator.Operator) squirrel.Sqlizer {
-		return squirrel.Expr(fmt.Sprintf("%s%s%s = ?", prefix, c.Field, doNot(o)), o.Value)
-	})
-}
-func NewUUIDOperator(field string, keys ...string) SearchOperatorConfigUUID {
-	return SearchOperatorConfigUUID{
-		StringSearchOperatorConfigBase{
-			SearchOperatorConfigBase{
-				Field: field, Keys: keys,
-			},
-		},
-	}
-}
-
-type SearchOperatorConfigStringLike struct {
-	StringSearchOperatorConfigBase
-}
-func (c SearchOperatorConfigStringLike) Sqlizer(o operator.Operator, prefix string) squirrel.Sqlizer {
-	return squirrel.Expr(fmt.Sprintf("%s%s%s ILIKE ?", prefix, c.Field, doNot(o)), "%"+o.Value+"%")
-}
-func (c SearchOperatorConfigStringLike) Apply(os, rem []operator.Operator, a *Accumulator, prefix string) {
-	fn := func(o operator.Operator) squirrel.Sqlizer {
-		return c.Sqlizer(o, prefix)
-	}
-	loopOperators(os, a, fn)
-	applyRemainders(rem, a, fn)
-}
-func NewStringLikeOperator(field string, keys ...string) SearchOperatorConfigStringLike {
-	return SearchOperatorConfigStringLike{
-		StringSearchOperatorConfigBase{
-			SearchOperatorConfigBase{
-				Field: field, Keys: keys,
-			},
-		},
-	}
-}
-
-func loopOperators(os []operator.Operator, a *Accumulator, sq func(o operator.Operator) squirrel.Sqlizer ) {
+func loopOperators(os []operator.Operator, a *Accumulator, sq func(o operator.Operator) squirrel.Sqlizer) {
 	for _, o := range os {
 		q := sq(o)
 		if o.Has(operator.ModifierOr) {
@@ -132,3 +75,13 @@ func doNot(o operator.Operator) string {
 	}
 	return ""
 }
+
+type SearchOperatorConfigBase struct {
+	Keys  []string
+	Field string
+}
+
+func (c SearchOperatorConfigBase) GetKeys() []string {
+	return c.Keys
+}
+
