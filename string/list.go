8ccf055730ed6a12146769aa5daf8cdaeff36fb6
Bryan Cua <bryan@luxe.technology>
2021-09-06 14:40:08 -0700

    String array index

--- string/list.go
+++ string/list.go
@@ -1,11 +1,15 @@
 package strutil
 
 func StringInList(xs []string, a string) bool {
-	for _, g := range xs {
-		if a == g {
-			return true
-		}
-	}
-	return false
+	return Index(xs, a) != -1
 }
 
+func Index(xs []string, a string) int {
+	for i, g := range xs {
+		if a == g {
+			return i
+		}
+	}
+	return -1
+
+}
